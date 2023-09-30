37d9cdecbf38049d90a7032713f34a2fa2f4c8b5
Heroboycloud Akindel <shakiratakindel@gmail.com>
2023-09-30 09:00:40 +0100

    modified:   index.js

--- index.js
+++ index.js
@@ -8,5 +8,24 @@ let file_to_read= fs.readFileSync(file);
 return file_to_read.toString();
 
 }
-module.exports= ReadFile;
+function ReadStream(file){
+// Create a Readable Stream
+var data= '';
+var readerStream = fs.createReadStream(file);
+readerStream.setEncoding('UTF8');
+// Handle stream events --> data, end, and error
+readerStream.on('data', function(chunk) {  data += chunk; });
+readerStream.on('end',function(){ return data; });
+readerStream.on('error', function(err){  console.log(err.stack); });
+return data;
+
+}
+
+
+
+
+
+
+
+module.exports= { ReadFile, ReadStream };
 // exported it
